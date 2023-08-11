d447108a6b2ddb0a754636acf95381131252e222
Jure Topolak <jure.topolak@gmail.com>
2023-08-11 22:05:51 +0200

    Init

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  build: {
+    transpile: ['trpc-nuxt']
+  },
+  devtools: { enabled: true }
+})
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { $client } = useNuxtApp()
+
+const hello = await $client.hello.useQuery({ text: 'client3' })
+</script>
+
+<template>
+  <div>
+    <p>{{ hello.data?.value?.greeting }}</p>
+    <p>test</p>
+  </div>
+</template>
